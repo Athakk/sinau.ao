726177ab2c041a2aeb1e9a253b98e6b4f23902b5
Athakk <atha0901@gmail.com>
2025-11-26 16:49:54 +0700

    add auth

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserKelasController;
 use App\Http\Controllers\UserSubjectController;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -25,3 +27,7 @@ Route::prefix('admin')->name('admin.')->group(function() {
         'userSubject' => UserSubjectController::class
     ]);
 });
+
+FacadesAuth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
